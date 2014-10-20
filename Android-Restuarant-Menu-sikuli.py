b8d82489f229ecc3698d9637c5f54c8c18ef8bf8
cesine <cesine@yahoo.com>
2014-10-20 12:16:31 -0400

    initial tests

---
+++ Android-Restuarant-Menu-sikuli.py
@@ -0,0 +1,10 @@
+click("1413820284743.png")
+click("1413820284743.png")
+click("1413820331917.png")
+click("1413820370114.png")
+click("1413820370114.png")
+find(Pattern("1413820973071.png").similar(0.92))
+
+click("1413820493678.png")
+
+click("1413820493678.png")
